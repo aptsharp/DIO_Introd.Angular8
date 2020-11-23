31c0ad8c0b28b960bc5bcd0820597782768dbd03
Afonso -aptsharp <aff-on@hotmail.com>
2020-11-23 20:38:07 -0300

    Criando componentes e construindo o projeto

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,39 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course'
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
-export class CourselistComponent{
+export class CourselistComponent implements OnInit {
+
+    course: Course[] = [];
+
+    ngOnInit(): void {
+        this.course = [{
+            id: 1,
+            nome: 'JavaSript',
+            imageUrl: '',
+            price: 100.99,
+            code: 'XPTO-99',
+            duration: '5h',
+            rating: 5.4,
+            releaseDate: 'Jan'
+        },
+        {
+            id: 2,
+            nome: 'C#.Completo',
+            imageUrl: '',
+            price: 100.99,
+            code: 'CSharp-99',
+            duration: '26h',
+            rating: 9.9,
+            releaseDate:'Nov'
+        }]
+
+    }
+
+
+
 
 }
---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course{
+    id: number;
+    nome: string;
+    imageUrl: string;
+    price: number;
+    code: string;
+    duration: string;
+    rating: number;
+    releaseDate: string;
+}
